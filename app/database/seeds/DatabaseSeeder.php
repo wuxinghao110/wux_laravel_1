b42558f4a8a16b7b284b7de988556d7be3fbbb23
吴兴 <wuxing305@pinganfang.com>
2014-12-08 17:42:05 +0800

    larv 1

---
+++ app/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+class DatabaseSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		Eloquent::unguard();
+		$this->call('ArticleTableSeeder');
+		$this->call('PageTableSeeder');
+		$this->call('SentrySeeder');
+		// $this->call('UserTableSeeder');
+	}
+
+}
